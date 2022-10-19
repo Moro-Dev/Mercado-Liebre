7d2ef6c2ae7dd48b808feb04246f5434c8343cc3
Moro-Dev <matirodriguez428@gmail.com>
2022-10-19 20:30:57 -0300

    cambios de puerto y json

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require('path');
 const publicFolferPath = path.resolve('public');
 app.use(express.static(publicFolferPath));
 
-const APP_PORT = 1234;
+const APP_PORT = process.env.APP_PORT || 3001
 app.listen(APP_PORT, () => {
-    console.log('Servidor funcionando en puerto ' + APP_PORT)
+    console.log(`Servidor corriendo en puerto + ${APP_PORT}`);
 });
 
 app.get('/', (req, res) => {
