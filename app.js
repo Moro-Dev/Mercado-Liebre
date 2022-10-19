15479991ee24ad0c43c946f2e58e6ac2349aa7ab
Moro-Dev <matirodriguez428@gmail.com>
2022-10-19 18:52:10 -0300

    modificando estilos en formularios

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.get ('/register', (req, res)=>{
     res.sendFile(path.resolve ('./Views/register.html'))
 });
 
+app.get('/crear-producto', (req, res) => {
+    res.sendFile(path.resolve('./views/crear-producto.html'))
+});
 
-app.get('*', (req,res)=>{
-    res.send('¡la página que estás buscando, no se encuentra!')
-}); 
+
